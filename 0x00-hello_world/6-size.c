2088b78d5f3d1a0697f54f73eb26110c80359558
Your Name <test@email.com>
2023-06-15 13:58:47 -0700

    Added a command to print variable size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *
+ *
+*/
+int main(void)
+{
+printf("Size of a char: %s byte(s)", sizeof(char));
+printf("Size of an int: %s byte(s)", sizeof(int));
+printf("Size of a long int: %s byte(s)", sizeof(long));
+printf("Size of a long long int: %s byte(s)", sizeof(long long int));
+printf("Size of a float: %s 4 byte(s)", sizeof(float));
+return (0);
+}
