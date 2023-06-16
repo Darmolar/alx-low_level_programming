51a4a983b56aafcf42aa2f78e4ab55e96b1bc8de
Your Name <test@email.com>
2023-06-16 14:15:19 -0700

    Added a script to show the last number of a digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,15 +3,18 @@
 #include <time.h>
 #include <string.h>
 
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/* Code to return last digit
+ * Return Always 0
+ */
 int main(void)
 {
-	int n, lastDigit;
+	int n; 
+	int lastDigit;
 	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastDigit = n % 10;
+	
 	if (lastDigit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
