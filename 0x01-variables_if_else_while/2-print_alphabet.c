b95bc3daba83fdf2cd162138fbd355cbb64b8d53
Your Name <test@email.com>
2023-06-16 15:04:31 -0700

    Added a C program to print all alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A program to print out all alphabets
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	for(i = 'a', i = 'z', i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
