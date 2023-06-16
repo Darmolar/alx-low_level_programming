a82295733125bfd27835d67c0fe018272ed4df05
Your Name <test@email.com>
2023-06-16 14:03:45 -0700

    Added a script to show the last number of a digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,17 +15,15 @@ int main(void)
 	lastDigit = n % 10;
 	if(lastDigit > 5)
 	{
-		message = "and is greater than 5";
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	}
 	else if(lastDigit == 0)
 	{
-		message = "and is 0";
+		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 	}
 	else if(lastDigit < 6 && lastDigit != 0)
 	{
-		message = "and is less than 6 and not 0";
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 	}
-	
-	printf("Last digit of %d is %d %s", n, lastDigit, message);
 	return (0);
 }
