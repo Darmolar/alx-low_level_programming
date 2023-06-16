2c64b537ade2b97896589554b54e2974d3035d4a
Your Name <test@email.com>
2023-06-16 13:40:18 -0700

    Added a script to show the last number of a digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n, lastDigit, numLength;
+	char message;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	numLength = = strlen(n);
+	lastDigit = atoi(&n[numLength - 1]);
+	if(lastDigit > 5)
+	{
+		message = "and is greater than 5";
+	}
+	if(lastDigit == 0)
+	{
+		message = "and is 0";
+	}
+	else if(lastDigit < 6 && lastDigit != 0)
+	{
+		message = "and is less than 6 and not 0";
+	}
+	
+	printf("Last digit of %d is %d %s", n, lastDigit, message);
+	return (0);
+}
