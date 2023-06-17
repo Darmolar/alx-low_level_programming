3f38410c5aeea1a6cd1bf46f2e6f044eeb1eefb7
Your Name <test@email.com>
2023-06-16 17:24:23 -0700

    last update

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - A program to print out all alphabets
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				for (l = k + 1; i < 10; l++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if (i != 9 || j != 8 || k != 9 || l != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
