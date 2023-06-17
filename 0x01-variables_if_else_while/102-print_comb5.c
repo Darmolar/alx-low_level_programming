83db7142aba3d8afd5f86ed15c7ae0b87cabe9c8
Your Name <test@email.com>
2023-06-16 17:46:40 -0700

    last update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,9 +7,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 1; j < 100; j++)
 		{
 			putchar((i / 10) + '0');
             		putchar((i % 10) + '0');
