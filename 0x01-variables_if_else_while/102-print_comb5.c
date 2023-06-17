c472c810a16bca944df131bc8ebe3f30556853c3
Your Name <test@email.com>
2023-06-16 17:44:04 -0700

    last update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,23 +11,16 @@ int main(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			for (k = 0; k < 10; k++)
-			{
-				for (l = 0; i < 10; l++)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(' ');
-					putchar(k + '0');
-					putchar(l + '0');
-					if (i != 9 || j != 8 || k != 9 || l != 9)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+			putchar((i / 10) + '0');
+            		putchar((i % 10) + '0');
+            		putchar(' ');
+            		putchar((j / 10) + '0');
+          	 	 putchar((j % 10) + '0');
 
-			}
+            		if (i != 99 || j != 99) {
+                		putchar(',');
+                		putchar(' ');
+            		}
 		}
 	}
 	putchar('\n');
