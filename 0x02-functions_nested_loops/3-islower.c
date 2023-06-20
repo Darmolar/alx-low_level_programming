b258d7328b97731cdc4a45e7ab9f477df324d3f9
Your Name <test@email.com>
2023-06-20 15:26:17 -0700

    Added a program to check if is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *_islower - A program to print outlower case
+ *Return: 0
+*/
+int _islower(int c)
+{
+	char i;
+	if(islower(i))
+	{
+		return 1;
+	}else{
+		return 0;
+	}
+}
