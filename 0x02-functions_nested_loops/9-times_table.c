cc0f53a1c23e13a17616276397718c25385596a7
Your Name <test@email.com>
2023-06-20 16:56:27 -0700

    A program to print out times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,22 +1,43 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * times_table - A program to print out times table
- *Return: 0
-*/
+  * times_table - Prints the 9 times table, starting with 0
+  *
+  * Return: empty output
+  */
 void times_table(void)
 {
-	int i;
-	int j, z;
+	int x, y, z, u, d;
 
-	for (j = 0; j < 10; j++)
+	for (x = 0; x < 10; x++)
 	{
-		for (i = 0; i < 10; i++)
+		for (y = 0; y < 10; y++)
 		{
-			z = i * j;
-			printf("%d,  ", z);
+			z = x * y;
+
+			if (z > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+
+				_putchar(z + '0');
+			}
 		}
-		printf("\n");
+
+		_putchar('\n');
 	}
 }
