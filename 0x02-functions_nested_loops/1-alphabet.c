1705b43e7214ab7cd58a1fbce83085ed317ad71c
Your Name <test@email.com>
2023-06-20 15:03:34 -0700

    Added a program to print a - z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,9 +8,15 @@ void print_alphabet(void)
 {
 	char i;
 
-	for(i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
