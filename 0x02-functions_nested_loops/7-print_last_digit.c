b5a1b4195e709de53324defd48955091dbebe706
Your Name <test@email.com>
2023-06-20 16:09:25 -0700

    A program to print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_last_digit -  A program to print last digit
+ *@c: A chracter value
+ *
+ * Return: 0
+*/
+int print_last_digit(int c)
+{
+	return (c%10);
+}
