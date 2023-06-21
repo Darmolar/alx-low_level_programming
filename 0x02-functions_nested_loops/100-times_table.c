ae6207f20d9c301a515c1293a40f6e7ef6bc53b3
Your Name <test@email.com>
2023-06-20 17:18:54 -0700

    Prints the n times table, starting with 0

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+  * print_times_table - Prints the n times table, starting with 0
+  *
+  * Return: empty output
+  */
+void print_times_table(int n)
+{
+	int x, y, z, u, d;
+	
+	if(n > 15 || n < 0)
+	{
+		return (0);
+		break;
+	}
+
+	for (x = 0; x < n; x++)
+	{
+		for (y = 0; y < n; y++)
+		{
+			z = x * y;
+
+			if (z > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+
+				_putchar(z + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -22,3 +22,5 @@ void print_to_98(int n);
 
 void jack_bauer(void);
 
+void print_times_table(int n);
+
