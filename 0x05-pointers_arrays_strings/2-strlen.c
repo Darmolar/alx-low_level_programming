bd28d15e929d606d240b35a8bee3638a07b9e82e
Your Name <test@email.com>
2023-06-26 17:28:11 -0700

    A function to return the length of a variabe

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,17 +3,18 @@
 /**
  * _strlen - A function to return the length of a variabe
  * @s: A pointer variable
- * Return : 0 or 1
+ * Return: 0 - 1
  */
 
 int _strlen(char *s)
 {
 	int strlens = 0;
 
-	while (*s != '\0') {
+	while (*s != '\0')
+	{
 		strlens++;
 		s++;
 	}
 
-	return strlens;
+	return (strlens);
 }
