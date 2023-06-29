b4719e98001296166beec1fbf82b40a3bb79cf68
Your Name <test@email.com>
2023-06-29 14:37:58 -0700

    A function to concatinate two string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - A function to concatinate string
+ * @dest: first varibale
+ * @src: second variable
+ * Return: result
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *result;
+
+	while (*dest != '\0')
+	{
+		*result = *dest;
+		dest++;
+		result++;
+	}
+
+	while (*src != '\0')
+	{
+		*result = *src;
+		src++;
+		result++;
+	}
+	*result = '\0';
+	printf("%s", result);
+	return (result);
+}
