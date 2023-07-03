9af7a1073d4766fc1b01f02340071694257ced25
Your Name <test@email.com>
2023-07-03 16:42:17 -0700

    A function to check for character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_strchr - A function to check for character
+ * @s: a pointer variable
+ * @c: a chracter variable
+ * 
+ * Return: a pointer to chracter @c
+ */
+
+char *_strchr(char *s, char c)
+{
+	while(*s)
+	{
+		if(*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if(c == '\0')
+		return (s);
+
+	return (NULL);
+}
