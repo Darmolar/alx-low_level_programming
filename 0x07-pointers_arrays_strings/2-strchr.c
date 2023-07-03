71458c14b2a17dd936e48a30626cd17659052f10
Your Name <test@email.com>
2023-07-03 16:43:31 -0700

    A function to check for character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * *_strchr - A function to check for character
  * @s: a pointer variable
  * @c: a chracter variable
  * 
  * Return: a pointer to chracter @c
+ * @s. Return NULL if the character isn't found
  */
 
 char *_strchr(char *s, char c)
