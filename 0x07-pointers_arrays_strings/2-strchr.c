36edd7db3bcf2a97bb46c8756665724465036243
Your Name <test@email.com>
2023-07-03 16:45:58 -0700

    A function to check for character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,21 +5,20 @@
  * *_strchr - A function to check for character
  * @s: a pointer variable
  * @c: a chracter variable
- * 
  * Return: a pointer to chracter @c
  * @s. Return NULL if the character isn't found
  */
 
 char *_strchr(char *s, char c)
 {
-	while(*s)
+	while (*s)
 	{
-		if(*s != c)
+		if (*s != c)
 			s++;
 		else
 			return (s);
 	}
-	if(c == '\0')
+	if (c == '\0')
 		return (s);
 
 	return (NULL);
